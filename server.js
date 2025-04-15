828a319701d5d79e848043b689516ee73800c153
Raul Jiminian <raul.e.jiminian@gmail.com>
2025-04-15 12:05:02 -0400

    finalized server and db

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 import db from "./db/connection.js";
 import express from "express";
-// import routes
-// import cors
+import routes from "./routes/index.js";
+import cors from "cors";
 import logger from "morgan";
 import chalk from "chalk";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(logger("dev"));
 
-// routes
+app.use("/api", routes);
 
 db.on("connected", () => {
   console.clear();
